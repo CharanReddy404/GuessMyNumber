e0d4d1cc1f0b47f134bae625c66c85acb94db1c5
Charan B <charanlucky6143@gmail.com>
2022-06-27 23:50:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let highScore = 0;
+let score = 20;
+let myNumber = random();
+function random() {
+    return Math.floor(Math.random() * 20 + 1);
+}
+
+document.querySelector(".check").addEventListener("click", function () {
+    let guessNo = document.querySelector(".guess").value;
+    if (guessNo == myNumber) {
+        changeValues(score, "Correct", "#60b347", myNumber);
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector(".highscore").innerText = `${score}`;
+        }
+    } else if (guessNo < myNumber) {
+        document.querySelector(".message").innerText = "Guess Higher";
+        document.querySelector(".score").innerText = `${--score}`;
+    } else {
+        document.querySelector(".message").innerText = "Guess Lower";
+        document.querySelector(".score").innerText = `${--score}`;
+    }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+    score = 20;
+    myNumber = random();
+    document.querySelector(".guess").value = "";
+    changeValues(score, "Start guessing...", "#222", "?");
+});
+
+function changeValues(setScore, setMsg, setBody, setNumber) {
+    document.querySelector(".score").innerText = setScore;
+    document.querySelector(".message").innerText = setMsg;
+    document.querySelector("body").style.backgroundColor = setBody;
+    document.querySelector(".number").innerText = setNumber;
+}
